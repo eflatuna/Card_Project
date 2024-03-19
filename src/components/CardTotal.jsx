b108021276cab81d3b8fcd532f5cbaee86ad0752
esra <esrabaskaya84@gmail.com>
2024-03-19 14:32:52 +0100

    Approuter

---
+++ src/components/CardTotal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardTotal = () => {
+	return <div>CardTotal</div>;
+};
+
+export default CardTotal;
---
+++ src/pages/Update.Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Update.Product = () => {
+  return (
+    <div>Update.Product</div>
+  )
+}
+
+export default Update.Product
